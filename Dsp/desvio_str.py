31fa8ded203f572518665e712c4ec4f7305cb272
PabloRO07 <pablito.09rodriguez@gmail.com>
2020-04-20 18:45:25 -0300

    Creacion de Filtro de Media Movil

--- Dsp/desvio_str.py
+++ Dsp/desvio_str.py
@@ -6,8 +6,8 @@ import valor_medio as vm
 
 
 def desvio_str(xn):
-    n=len(xn)
-    v_m=vm.valor_medio(xn)
-    sigma=np.sqrt((1/(n-1))*np.sum((np.square(abs(xn-v_m)))))
-    sigma2=sigma**2
-    return(sigma,sigma2)
+    n = len(xn)
+    v_m = vm.valor_medio(xn)
+    sigma = np.sqrt((1/(n-1))*np.sum((np.square(abs(xn-v_m)))))
+    sigma2 = sigma**2
+    return sigma, sigma2
