27224536e9cac14339f7e73d49f6bbeff85cac96
PabloRO07 <pablito.09rodriguez@gmail.com>
2020-05-04 10:27:35 -0300

    COrrecines Tabla

--- Dsp/ruidito.py
+++ Dsp/ruidito.py
@@ -12,32 +12,30 @@ ruidito10k = np.random.normal(0, 1, 10000)
 ruidito100k = np.random.normal(0, 1, 100000)
 
 "Calculo del Desvio Estandar para cada uno delos ruidos generados"
-desvio5 = ds.desvio_str(ruidito5)[0]
-desvio10 = ds.desvio_str(ruidito10)[0]
-desvio100 = ds.desvio_str(ruidito100)[0]
-desvio1k = ds.desvio_str(ruidito1k)[0]
-desvio10k = ds.desvio_str(ruidito10k)[0]
-desvio100k = ds.desvio_str(ruidito100k)[0]
+desvio5 = ds.desvio_str(ruidito5)
+desvio10 = ds.desvio_str(ruidito10)
+desvio100 = ds.desvio_str(ruidito100)
+desvio1k = ds.desvio_str(ruidito1k)
+desvio10k = ds.desvio_str(ruidito10k)
+desvio100k = ds.desvio_str(ruidito100k)
 
-"Vector con lso devios"
+"Vector con los devios"
 des = np.array([desvio5, desvio10, desvio100, desvio1k, desvio10k, desvio100k])
+porc = np.absolute(1-des)
+porc = (porc/des)*100
+print(porc.size)
+print(des.size)
 
-desv = abs(1-des)
-desv = (desv/des)*100
+"Creacion de Tabla"
+fig, ax = plt.subplots()
+# hide axes
+fig.patch.set_visible(False)
+ax.axis('off')
+ax.axis('tight')
 
-t = [des, desv]
-
-# "Creacion de Tabla"
-# fig, ax = plt.subplots()
-# # hide axes
-# fig.patch.set_visible(False)
-# ax.axis('off')
-# ax.axis('tight')
-#
-# colLabels = ("Varianza", "%")
-# rowLabels = ("5", "10", "100", "1k", "10k", "100k")
-# data = t
-#
-# ax.table(cellText=data, colLabels=colLabels, loc='center', rowLabels=rowLabels)
-# fig.tight_layout()
-# plt.show()
+colLabels = ("Varianza", "%")
+rowLabels = ("5", "10", "100", "1k", "10k", "100k")
+data = des
+ax.table(cellText=data, colLabels=colLabels, loc='center', rowLabels=rowLabels)
+fig.tight_layout()
+plt.show()
