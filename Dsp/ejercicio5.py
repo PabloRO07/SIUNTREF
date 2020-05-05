c2fe0619e162821870f06311fa59acd3280c5040
martinflaiban <flaibanmartin@gmail.com.ar>
2020-05-05 14:23:26 -0300

    punto 6 todavia falta terminar

--- Dsp/ejercicio5.py
+++ Dsp/ejercicio5.py
@@ -5,28 +5,28 @@ from punto5 import fmm as fmm
 from punto5 import frmm as frmm
 from timeit import default_timer
 
-"""Se procede en este scrip a comparar el tiempo de ejecucion de:
+"""Se procede en este script a comparar el tiempo de ejecucion de:
         Filtro de media movil
         Filtro de media movil recursivo
     Para una señal de senoidal con los siguiente parametros
         Frecuencia de muestreo:44100
-        Frecuencia de la señal:1kHz
+        Frecuencia de la señal:10kHz
         Duracion de la señalÑ 0.5 seg
-        Aplitud: 1
+        Amplitud: 1
         Valor de continua/Centrado : 2
 """
 # Definimos xt como la señal de entrada
-[xt, T] = sg.singenerator(44100, 1000, 0.5, 1, 2)
+[xt, T] = sg.singenerator(44100, 10000, 0.5, 1, 2)
 
 # Aplicamos los filtros y medimos lso timepos
 'Para filtro de media movil'
 startfmm = default_timer()
-filtro = fmm.fmm(xt, 3)
+filtro = fmm.fmm(xt, 2)
 finfmm = default_timer()
 ejecucionfmm = finfmm - startfmm
 'Para filtro recursivo de de media movil'
 startfrmm = default_timer()
-recursive = frmm.frmm(xt, 3)
+recursive = frmm.frmm(xt, 2)
 finfrmm = default_timer()
 ejecucionfrmm = finfrmm - startfrmm
 
@@ -37,7 +37,6 @@ print('El tiempo de ejecucion para el filto Recursivo de media movil fue: ', eje
 plt.style.use('seaborn')
 
 fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, ncols=1, sharex=True)
-
 ax1.plot(T, xt, color='r')
 ax2.plot(T, filtro, color='y')
 ax3.plot(T, recursive)
@@ -47,10 +46,11 @@ ax1.set_ylabel('Amplitud')
 ax1.set_xlabel('Time[s]')
 
 ax2.set_title('Filtada con filtro de media movil')
-ax2.set_ylabel('Amplitude')
+ax2.set_ylabel('Amplitud')
 
-ax3.set_title('RFMM FIlter')
+ax3.set_title('Filtro de media movil recursivo')
 ax3.set_xlabel('Time[s]')
+ax3.set_ylabel('Amplitud')
 
 plt.tight_layout()
 plt.show()
--- Dsp/ejercicio6.py
+++ Dsp/ejercicio6.py
@@ -1,23 +1,48 @@
 import numpy as np 
 from matplotlib import pyplot as plt 
-import RFMM as rfmm
-import FMM as fmm
+from punto1 import singenerator as sg
+from punto5 import fmm as fmm
+from punto5 import frmm as frmm
+from timeit import default_timer
 def FMM_conv(x,m):
     kernel= np.ones(m)/(m+1)
     salida=np.convolve(x,kernel)
     return(salida)
 
-t=0.5
-fs=44100
-t=np.linspace(0,t,int(fs*t))
-f=10000
-xt= 2 + np.sin(2*np.pi*f*t)
-ruidito3 = np.random.normal(0,3,len(t)) #Creo la señal del punto 3 para compararla
+# Definimos xt como la señal de entrada
+[xt, T] = sg.singenerator(44100, 10000, 0.5, 1, 2)
+
+ruidito3 = np.random.normal(0,3,len(T)) #Creo la señal del punto 3 para compararla
 x3= (ruidito3+xt) 
 m=round(0.01*len(x3))
+# Aplicamos los filtros y medimos los timepos
+'Para filtro de media movil por convolución'
+startFMM_conv = default_timer()
 señal_filtrada=FMM_conv(x3,m)
-señal_filtrada2=rfmm.RFMM(x3,m)
-señal_filtrada3=fmm.FMM(x3,m)
+finFMM_conv = default_timer()
+ejecucionFMM_conv = finFMM_conv - startFMM_conv
+###################################
+'Para filtro de media movil.'
+startfmm = default_timer()
+señal_filtrada2=frmm.frmm(x3,m)
+finfmm=default_timer()
+ejecucionfmm= finfmm - startfmm
+#################################
+'Para filtro de media movil recursivo.'
+startfrmm=default_timer()
+señal_filtrada3=fmm.fmm(x3,m)
+finfrmm=default_timer()
+ejecucionfrmm= finfrmm - startfrmm
+####################################
+
+print('El tiempo de ejecucion para el filto de media movil fue: ', ejecucionFMM_conv, 'Segundos')
+print('El tiempo de ejecucion para el filto Recursivo de media movil fue: ', ejecucionfmm, 'Segundos')
+print('El tiempo de ejecucion para el filto Recursivo de media movil fue: ', ejecucionfrmm, 'Segundos')
+####################################
+'Normalizado de los vectores.'
+señal_filtrada= señal_filtrada/ abs(max(señal_filtrada))
+señal_filtrada2= señal_filtrada2/ abs(max(señal_filtrada2))
+señal_filtrada3= señal_filtrada3/ abs(max(señal_filtrada3))
 
 
 t2=np.linspace(0,0.5,len(señal_filtrada))
@@ -25,8 +50,8 @@ t2=np.linspace(0,0.5,len(señal_filtrada))
 
 fig, axs = plt.subplots(4)
 fig.suptitle('Filtro de media movil')
-axs[0].plot(t,x3)
+axs[0].plot(T,x3)
 axs[1].plot(t2,señal_filtrada)
-axs[2].plot(t,señal_filtrada2)
-axs[3].plot(t,señal_filtrada3)
+axs[2].plot(T,señal_filtrada2)
+axs[3].plot(T,señal_filtrada3)
 plt.show()
