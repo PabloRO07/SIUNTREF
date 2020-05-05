501509e6542b2935877a6182b3cbde5664504823
martinflaiban <flaibanmartin@gmail.com.ar>
2020-05-05 19:14:26 -0300

    punto 6y7 casi terminados.

--- Dsp/Ejercicio7.py
+++ Dsp/Ejercicio7.py
@@ -4,17 +4,29 @@ from punto1 import singenerator as sg
 from punto5 import fmm as fmm
 from punto5 import frmm as frmm
 from timeit import default_timer
-def FMM_conv(x,m):
-    kernel= np.ones(m)/(m+1)
-    salida=np.convolve(x,kernel)
-    return(salida)
+from Ejercicio6 import FMM_conv
+def black_w(xt,m):
+    a0 = 0.42
+    a1 = 0.5
+    a2 = 0.08
+    kernel=np.zeros(m)
+    
+    i=np.arange(m-1)
+    kernel = a0 - a1*np.cos((2*np.pi*i)/(m-1))+a2*np.cos((4*np.pi*i)/(m-1))
+    y=np.convolve(xt,kernel)
+    return(y)
+
 
 # Definimos xt como la señal de entrada
-[xt, T] = sg.singenerator(44100, 10000, 0.5, 1, 2)
+[xt, t] = sg.singenerator(44100, 10000, 0.5, 1, 2)
 
-ruidito3 = np.random.normal(0,3,len(T)) #Creo la señal del punto 3 para compararla
-x3= (ruidito3+xt) 
-m=round(0.01*len(x3))
+ruidito3 = np.random.normal(0,3,len(t)) #Creo la señal del punto 3 para compararla
+x3= (ruidito3+xt)
+m=round(0.01*len(x3)) # ventana del 1%
+
+
+
+
 # Aplicamos los filtros y medimos los timepos
 'Para filtro de media movil por convolución'
 startFMM_conv = default_timer()
@@ -35,30 +47,41 @@ señal_filtrada3=fmm.fmm(x3,m)
 finfmm=default_timer()
 ejecucionfmm= finfmm - startfmm
 ####################################
+'Para filtro de media movil con ventana Blackman .'
+startblack=default_timer()
+señal_filtrada4=black_w(x3,m) 
+finblack=default_timer()
+ejecucionblack = finblack - startblack
 
 print('El tiempo de ejecucion para el filtro de media movil por convolución fue: ', ejecucionFMM_conv, 'Segundos')
 print('El tiempo de ejecucion para el filtro de media movil fue: ', ejecucionfmm, 'Segundos')
 print('El tiempo de ejecucion para el filtro Recursivo de media movil fue: ', ejecucionfrmm, 'Segundos')
+print('El tiempo de ejecucion para el filtro Recursivo de media movil con ventana blackman fue: ', ejecucionblack, 'Segundos')
 ####################################
 'Normalizado de los vectores.'
 señal_filtrada= señal_filtrada/ abs(max(señal_filtrada))
 señal_filtrada2= señal_filtrada2/ abs(max(señal_filtrada2))
 señal_filtrada3= señal_filtrada3/ abs(max(señal_filtrada3))
+señal_filtrada4= señal_filtrada4/ abs(max(señal_filtrada4))
 
+#####################
+'Vectores tiempo para el plot'
 muestras_tfmm=(len(señal_filtrada2))
 muestras_tfrmm=(len(señal_filtrada3))
 muestras_tconv=len(x3)+m-1
+muestras_tblack=len(señal_filtrada4)
 t_conv=np.linspace(0,0.5,muestras_tconv)
 tfmm=np.linspace(0,0.5,muestras_tfmm)
 tfrmm=np.linspace(0,0.5,muestras_tfrmm)
-
+tblack=np.linspace(0,0.5,muestras_tblack)
 # Plot
 plt.style.use('seaborn')
 
-fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, ncols=1, sharex=True)
+fig, (ax1, ax2, ax3, ax4) = plt.subplots(nrows=4, ncols=1, sharex=True)
 ax1.plot(t_conv ,señal_filtrada, color='r')
 ax2.plot(tfmm, señal_filtrada2, color='y')
 ax3.plot(tfrmm, señal_filtrada3)
+ax4.plot(tblack, señal_filtrada4, color='g')
 
 ax1.set_title('Filtrado por convolución')
 ax1.set_ylabel('Amplitud')
@@ -71,9 +94,10 @@ ax3.set_title('Filtro de media movil')
 ax3.set_xlabel('Time[s]')
 ax3.set_ylabel('Amplitud')
 
+ax4.set_title('Filtro de media movil con ventana blackman')
+ax4.set_xlabel('Time[s]')
+ax4.set_ylabel('Amplitud')
+
 plt.tight_layout()
 plt.show()
 
-#[m:muestras_tconv-m] ESTO ES POR SI QUIERO RECORTAR EL VECTOR POR LOS ZEROS que se forman al promediar las muestras d la señal.
-#[0:muestras_tfmm]
-#[0:muestras_tfrmm] PREGUNTAR AL PROFE SI EL PLOT TA BIEN O SI LO PUEDO DEJAR RECORTADO + lindo.
