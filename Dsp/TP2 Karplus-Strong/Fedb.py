328c4c85366bb03b9ca5e206e62604b4fe22596d
Pablo <53527633+PabloRO07@users.noreply.github.com>
2020-07-16 11:49:03 -0300

    Push para la marta

---
+++ Dsp/TP2 Karplus-Strong/Fedb.py
@@ -0,0 +1,41 @@
+import numpy as np
+import soundfile as sf
+
+
+def fdb(xn, a, d, af, n):
+    """Entry Parameters
+    xn = Signal Entry
+    a = Amplitude delayed signal
+    d = Delay time
+    af = Amplitude feedback signal
+    n = Quantity of delays
+    """
+    l = len(xn)
+    for i in range(n):
+        if i == 0:
+            delay = a*(np.hstack((np.zeros(d), xn)))
+            delay2 = af * (np.hstack((np.zeros(d), delay)))
+            delay = np.hstack((delay, np.zeros(d)))
+            xn = np.hstack((xn, np.zeros(2*d)))
+            fdb = xn+delay+delay2
+        else:
+            delay = a*(np.hstack((np.zeros(d), fdb)))
+            delay2 = af*(np.hstack((np.zeros(d), delay)))
+            delay = np.hstack((delay, np.zeros(d)))
+            fdb = np.hstack((fdb, np.zeros(2*d)))
+            fdb = fdb + delay + delay2
+    return fdb
+
+
+fs = 44100
+xn, fs = sf.read('Midi69.wav')
+'cantidad de repeticiones'
+n = 10
+'Muestas atrazadas'
+d = round(0.5 * fs)
+'Amplitud de los delays'
+a = 0.5
+'Amplitud de la realimentacion'
+af = 0.5
+synth = fdb(xn, a, d, af, n)
+sf.write('echo.wav', synth, fs)
--- Dsp/prueba 2.py
+++ Dsp/prueba 2.py
@@ -3,4 +3,4 @@ from matplotlib import pyplot as plt
 
 fs = 44100
 t = np.linspace(0, fs)
-print(t)
+print(1%1)
