998661475d8c15f2d073a0342294a891f5f7e6de
Pablo <53527633+PabloRO07@users.noreply.github.com>
2020-07-18 11:28:59 -0300

    act

--- Dsp/TP2 Karplus-Strong/Fedb.py
+++ Dsp/TP2 Karplus-Strong/Fedb.py
@@ -11,7 +11,6 @@ def fdb(xn, a, d, af, n, fs):
     af = Amplitude feedback signal
     n = Quantity of feedbacks
     """
-    d = round(0.5 * fs)
     l = len(xn)
     for i in range(n):
         if i == 0:
@@ -34,7 +33,7 @@ xn, fs = sf.read('Midi69.wav')
 'cantidad de repeticiones'
 n = 1
 'Muestas atrazadas'
-d = round(0.5 * fs)
+d = round(4 * fs)
 'Amplitud de los delays'
 a = 0.5
 'Amplitud de la realimentacion'
