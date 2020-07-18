86327eb75462ad8a6e38604e7f112d1144387266
martinflaiban <flaibanmartin@gmail.com.ar>
2020-07-18 00:28:00 -0300

    tp2

--- Dsp/TP2 Karplus-Strong/TP2/Feedb.py
+++ Dsp/TP2 Karplus-Strong/TP2/Feedb.py
@@ -1,8 +1,6 @@
 import numpy as np
-import soundfile as sf
 
-
-def fdb(xn, a, d, af, n, fs):
+def feedb(xn, a, d, af, n):
     """Entry Parameters
     This function recive a entry signal and creates "n" feedbacks
     xn = Signal Entry
@@ -11,7 +9,6 @@ def fdb(xn, a, d, af, n, fs):
     af = Amplitude feedback signal
     n = Quantity of feedbacks
     """
-    d = round(0.5 * fs)
     l = len(xn)
     for i in range(n):
         if i == 0:
@@ -25,19 +22,7 @@ def fdb(xn, a, d, af, n, fs):
             delay2 = af*(np.hstack((np.zeros(d), delay)))
             delay = np.hstack((delay, np.zeros(d)))
             fdb = np.hstack((fdb, np.zeros(2*d)))
-            fdb = fdb + delay + delay2
+            xn = np.hstack((xn, np.zeros(2*d)))
+            fdb = xn+fdb + delay + delay2
+            fdb = fdb/abs(max(fdb))
     return fdb
-
-
-fs = 44100
-xn, fs = sf.read('Midi69.wav')
-'cantidad de repeticiones'
-n = 1
-'Muestas atrazadas'
-d = round(0.5 * fs)
-'Amplitud de los delays'
-a = 0.5
-'Amplitud de la realimentacion'
-af = 0.5
-synth = fdb(xn, a, d, af, n, fs)
-sf.write('echo.wav', synth, fs)
--- Dsp/TP2 Karplus-Strong/TP2/TP2_root.py
+++ Dsp/TP2 Karplus-Strong/TP2/TP2_root.py
@@ -3,8 +3,9 @@ from matplotlib import pyplot as plt
 import soundfile as sf 
 import scipy as sc 
 import delay4 as dl4
+import Feedb as fdb
 
-#####################################################
+# Este es el Scrypt raiz de los 4 sistemas propuestos por el trabajo.
 
 #Sistema 1 echos finitos
 
@@ -17,7 +18,7 @@ alfa=(0.5) # amplitud de los echos
 audio,fs = sf.read('Midi69.wav') # Audio de prueba
 synt = dl4.delay4(audio,d2,alfa) 
 
-sf.write('prueba_4echo.wav',synt,fs) # archivo wav.
+sf.write('Sistema_1.wav',synt,fs) # archivo wav.
 
 ### Analisis del sistema ###
 impulse=np.zeros(fs-1) # creo vector de ceros para el impulso
@@ -28,18 +29,19 @@ imag=np.imag(transfer)
 real=np.real(transfer)
 frecuency_response=abs(transfer) # respuesta en Frecuencia 
 phase= np.arctan((imag/real))
-t1=np.linspace(0,(len(audio)/fs),len(audio))
-t2=np.linspace(0,(len(synt)/fs),len(synt))
+t1=np.linspace(0,(len(audio)/fs),len(audio)) # Vector tiempo x[n]
+t2=np.linspace(0,(len(synt)/fs),len(synt)) # Vector tiempo para señal Y2[n]
 
 plt.style.use('seaborn') # ploteos del sistema 1
-fig, ((ax1,ax2 ),(ax3,ax4)) = plt.subplots(nrows=2, ncols=2,sharex='col')
+fig, ((ax1,ax2 ),(ax3,ax4)) = plt.subplots(nrows=2, ncols=2,sharex='col',figsize=(16, 10))
+fig.suptitle(r'$ Analisis \ del \ sistema \ 1 \ ecos \ finito $',fontsize=18,fontname="Times New Roman Bold")
 ax1.plot(frecuency_response[0:round(fs/2)], color='b',label=(r'$D=4$'))
 ax2.plot(t1,audio, color='black')
 ax3.plot(phase[0:round(fs/2)], color='b',label=(r'$D=4$'))
 ax4.plot(t2,synt, color='black',label=(r'$ S= 500 ms $'))
 
 
-ax1.set_title(r'$ |H_1(e^{jw})| \ Sistema 1 $',fontsize=14)
+ax1.set_title(r'$ |H_1(e^{jw})| $',fontsize=14)
 ax1.set_ylabel(r'$Amplitud$',fontsize=14)
 
 
@@ -52,7 +54,7 @@ ax3.set_ylabel(r'$Amplitud$',fontsize=14)
 ax3.set_xlabel(r'$Frecuencia [Hz]$',fontsize=14)
 
 
-ax4.set_title(r'$Señal \ de \ salida \ y[n]  $',fontsize=14)
+ax4.set_title(r'$Señal \ de \ salida \ y_1[n]  $',fontsize=14)
 ax4.set_ylabel(r'$Amplitud$',fontsize=14)
 ax4.set_xlabel(r'$Tiempo [s]$',fontsize=14)
 
@@ -61,7 +63,62 @@ plt.legend(fontsize=12)
 plt.tight_layout()
 plt.show()
 
-########################################################################
-
 
 #Sistema 2 Feedback delay
+
+d=4 # Retraso en muestras , para gráfico de respuesta en frecuencia y fase.
+s= 0.5 # tiempo en segundos , para señal de prueba 
+d2=round(s*44100) # retraso para audio de prueba
+alfa=(0.5) # amplitud de los echos
+af = 0.5 # amplitud del feedback
+n=10 #numero de repeticiones del feedback
+
+synth = fdb.feedb(audio, alfa, d2, af, n)
+
+sf.write('Sistema_2.wav', synth, fs)
+
+
+### Analisis del sistema 2 ###
+n2=1 # para el analisis del sistema
+impulse=np.zeros(fs-1) # creo vector de ceros para el impulso
+impulse[0]=1
+synth2 = fdb.feedb(impulse, alfa, d, af, n2) # respuesta al impulso del sistema
+transfer2=sc.fft.fft(synth2) #FFT de la respuesta
+imag2=np.imag(transfer2)
+real2=np.real(transfer2)
+frecuency_response2=abs(transfer2) # respuesta en Frecuencia del sistema 2
+phase2= np.arctan((imag2/real2)) # Respuesta de fase del sistema 2
+t1=np.linspace(0,(len(audio)/fs),len(audio)) # Vector tiempo x[n]
+t2=np.linspace(0,(len(synth)/fs),len(synth)) # Vector tiempo para señal Y2[n]
+
+plt.style.use('seaborn') # ploteos del sistema 2
+fig, ((ax1,ax2 ),(ax3,ax4)) = plt.subplots(nrows=2, ncols=2,sharex='col')
+fig.suptitle(r'$ Analisis \ del \ sistema \ 2 \ Feedback \ delay $',fontsize=18,fontname="Times New Roman Bold")
+ax1.plot(frecuency_response2[0:round(fs/2)], color='b',label=(r'$D=4$'))
+ax2.plot(t1,audio, color='black')
+ax3.plot(phase2[0:round(fs/2)], color='b',label=(r'$D=4$'))
+ax4.plot(t2,synth, color='black',label=(r'$ S= 500 ms $'))
+
+
+ax1.set_title(r'$ |H_2(e^{jw})| $',fontsize=14)
+ax1.set_ylabel(r'$Amplitud$',fontsize=14)
+
+
+ax2.set_title(r'$Señal \ Original \ x[n] $',fontsize=14)
+ax2.set_ylabel(r'$Amplitud$',fontsize=14)
+
+
+ax3.set_title(r'$Fase \ \angle \ H_2(e^{jw}) $',fontsize=14)
+ax3.set_ylabel(r'$Amplitud$',fontsize=14)
+ax3.set_xlabel(r'$Frecuencia [Hz]$',fontsize=14)
+
+
+ax4.set_title(r'$Señal \ de \ salida \ y_2[n] \ Normalizada  $',fontsize=14)
+ax4.set_ylabel(r'$Amplitud$',fontsize=14)
+ax4.set_xlabel(r'$Tiempo [s]$',fontsize=14)
+
+
+plt.legend(fontsize=12)
+plt.tight_layout()
+plt.show()
+
