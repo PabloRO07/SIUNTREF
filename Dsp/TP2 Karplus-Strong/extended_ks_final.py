a2afb6c1bc4aba43ca916bcbe9d3e22e005ceced
Pablo <53527633+PabloRO07@users.noreply.github.com>
2020-07-22 00:57:49 -0300

    asd

--- Dsp/TP2 Karplus-Strong/extended_ks_final.py
+++ Dsp/TP2 Karplus-Strong/extended_ks_final.py
@@ -4,12 +4,16 @@ import soundfile as sf
 import scipy as sc 
 import karplus_final as ks
 
-def extended_ks(f, fs, n, b,bw,rho,beta):
+
+def extended_ks(f, fs, n, b, bw, rho, beta):
     """
     :param f: Frecuency to synthesize
     :param fs: Sampling frequency
     :param n: Long time in second of the output signal
     :param b: Probabilistic parameter b>9 for string synthesizing  and 0>=b<9 for drum synthesizing
+    :param bw: Cur Frequency Dynamic Low pass Filter
+    :param rho: Pick direction Low pass filter From 0= no effect to 1
+    :param beta: Pick position Comb filter from 0= no effect, to 0.5
     :return: Signal of "f" frequency, drum or string, of time "n"
     """
     n = n*fs
@@ -21,23 +25,23 @@ def extended_ks(f, fs, n, b,bw,rho,beta):
     karplus = np.zeros(n)
     
 # Pick direction Low pass filter  Hf
-    l=len(wavetable) 
-    y=0
-    acumulador=0
+    l = len(wavetable)
+    y = 0
+    acumulador = 0
     wavetable_pick = np.zeros(l)
-    if (rho!=0):
+    if rho != 0:
         for i in range(l):
-            wavetable_pick[i]= wavetable[i] -rho*( wavetable[i]-acumulador)
-            acumulador=wavetable_pick[i]
-        wavetable=wavetable_pick
+            wavetable_pick[i] = wavetable[i] - rho*(wavetable[i]-acumulador)
+            acumulador = wavetable_pick[i]
+        wavetable = wavetable_pick
 # Pick position Comb filter He
-    wavetable_d=np.hstack((np.zeros(round(l*beta)),wavetable))
-    wavetable_comb_pick=np.zeros(l)
-    a1=0
+    wavetable_d = np.hstack((np.zeros(round(l*beta)), wavetable))
+    wavetable_comb_pick = np.zeros(l)
+    a1 = 0
     for i in range(l):
-        wavetable_comb_pick[i]=wavetable[a1]-wavetable_d[i]
-        a1+=1
-        a1= a1 % len(wavetable) 
+        wavetable_comb_pick[i] = wavetable[a1]-wavetable_d[i]
+        a1 += 1
+        a1 = a1 % len(wavetable)
     
     a = 0
     for i in range(n):
@@ -52,18 +56,17 @@ def extended_ks(f, fs, n, b,bw,rho,beta):
         a += 1
         a = a % len(wavetable)
     
-    #Dynamic Low pass Filter  Butterworth first order Hd
-    if bw!=0:
-        r=np.exp(-np.pi*bw*(2/fs))
+    # Dynamic Low pass Filter  Butterworth first order Hd
+    if bw != 0:
+        r = np.exp(-np.pi*bw*(2/fs))
         for i in range(n):
-            karplus[i]= (1-r)*karplus[i] +r*y
-            y=karplus[i]
-
+            karplus[i] = (1-r)*karplus[i] + r*y
+            y = karplus[i]
 
     return karplus
 
 
-karplus=extended_ks(100, 12000, 1, 1,0,0,0.5)
-sf.write('prueba_dinamic_filter2.wav',karplus,44100)
-karplus2=ks.karplus(100, 44100, 1, 1)
-sf.write('prueba_dinamic_filter.wav',karplus2,44100)
+karplus = extended_ks(100, 12000, 1, 1, 0, 0, 0.5)
+sf.write('prueba_dinamic_filter2.wav', karplus, 44100)
+karplus2 = ks.karplus(100, 44100, 1, 1)
+sf.write('prueba_dinamic_filter.wav', karplus2, 44100)
